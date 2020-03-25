2956f749116bedf5a815e218118f2b616c8df8eb
jan.kowalski <kvartez@gmail.com>
2020-03-25 14:46:12 +0100

    dupa

---
+++ magsain.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct uczan{
+	public:
+	string imie;
+	string nazwisko;
+	float wiek;
+	uczan(string im,string nazw ,float wi){
+		imie =im;
+		nazwisko= nazw;
+		wiek=wi;
+		
+	}
+};
+void wypisz(struct uczan dup){
+	cout<< dup.imie<<endl;
+	cout<< dup.nazwisko<<endl;
+	cout<< dup.wiek<<endl;
+}
+
+int main(int argc, char** argv) {
+	uczan p1("milosz","gwizdziel",16);
+	wypisz(p1);
+	uczan p2("grzybnik","rudnik",16);
+	wypisz (p2);
+	uczan p3("bambaryla","ludzka",15);
+	wypisz (p3);
+	return 0;
+}
